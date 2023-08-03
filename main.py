7bcaeef08ece431898e33c21c4e6a2dd0b678f19
NazarGniluh <140846129+NazarGniluh@users.noreply.github.com>
2023-08-03 17:15:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import *
+import random
+
+
+app = QApplication([])
+
+window = QWidget()
+window.resize(500, 500)
+
+firstText = QLabel("Тицни, щоби дізнатися переможця!")
+twoText = QLabel("Переможець")
+knopkf = QPushButton("Визначити переможця")
+
+
+line = QVBoxLayout()
+line.addWidget(firstText)
+
+
+line.addWidget(twoText)
+line.addWidget(knopkf)
+
+window.setLayout(line)
+
+def winner():
+    a = str(random.randint(0, 100))
+    twoText.setText(a)
+
+
+knopkf.clicked.connect(winner)
+
+window.show()
+
+app.exec()
